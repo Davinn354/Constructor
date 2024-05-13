3e92ebf3e891d80cbece9a48c708da05e09f68a1
Davinn354 <daviipad53@gmail.com>
2024-05-13 14:39:40 +0700

    Penambahan int

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -7,5 +7,14 @@ public:
     static int nim;
     int id;
     string nama;
+
+    void setID();
+
+    void printAll();
+    mahasiswa(string pnama):nama(pnama)
+    {
+        setID();
+    }
 };
 
+int mahasiswa::nim = 20;
