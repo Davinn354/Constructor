d01ba2254c9bb0c0467bbed1f9b7b206d0b0156d
Davinn354 <daviipad53@gmail.com>
2024-05-13 16:46:03 +0700

    Peambahan class buku

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 class buku
+{
+    string judul;
+
+public:
+    buku setJdul(string judul)
+    {
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
