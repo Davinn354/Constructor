ea348e59a23d2a607ac76a43b084ac4a4187f7b5
Davinn354 <daviipad53@gmail.com>
2024-05-13 15:54:38 +0700

    Penambahan int main

--- construktor.cpp
+++ construktor.cpp
@@ -18,5 +18,40 @@ public:
         nim = iNim;
 
     };
-    
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+
+    void cetak()
+    {
+        cout <<"NIM  = " << nim << endl;
+        cout << "Nama  = " << nama << endl;
+        cout << endl;
+    }
+
+};
+
+Mahasiswa::Mahasiswa(string iNama){
+    nama = iNama;
+};
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+
+int main()
+{
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(200);
+    Mahasiswa mhs3("Sri Mulyado");
+    Mahasiswa mhs4(10, "Joko" );
+
+    mhs1.cetak();
+
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
 };
