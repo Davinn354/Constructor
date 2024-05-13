660d142ffca7acd6f940c235fa13ee554d2aa480
Davinn354 <daviipad53@gmail.com>
2024-05-13 16:56:40 +0700

    Penambahan ckass angka

--- destruktor.cpp
+++ destruktor.cpp
@@ -5,4 +5,22 @@ class angka{
 private:
     int *arr;
     int panjang;
+public:
+    angka(int);
+    ~angka();
+    void cetakData();
+    void isiData();
 };
+//Definisi member Function
+angka::angka(int i){// constructor
+    panjang = 1;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka(){
+    cout<<endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
